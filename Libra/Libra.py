c9976ef9f0e925b7e3a30065226034c51929185c
jartsanull <langinenj1@gmail.com>
2024-07-08 23:02:42 +0300

    lol

--- Libra/Libra.py
+++ Libra/Libra.py
@@ -1,6 +1,9 @@
 import time
 import funcs
 
+book_list = ["Bible", "The Headless Horseman", "The Gospel Driven Life", "On the Incarnation"]
+global book_list
+
 print("Hey!")
 time.sleep(1.5)
 print("How can I help you?")
@@ -8,4 +11,4 @@ print("How can I help you?")
 
 
 
-service()
+bot()
--- Libra/funcs.py
+++ Libra/funcs.py
@@ -1,1 +1,11 @@
-def service():
+import Libra
+from Libra.Libra import book_list
+import time
+
+def service(choice: str):
+    match(choice):
+        case "recommendation":
+            print("Oh, I can recommend you these books:")
+            print(book_list)
+
+def bot():
