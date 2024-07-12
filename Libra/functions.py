2d7b03fd785c56d21620a75e4c050ee5ddf5584f
jartsanull <langinenj1@gmail.com>
2024-07-13 00:34:02 +0300

    lol

--- Libra/functions.py
+++ Libra/functions.py
@@ -3,6 +3,7 @@ import datetime
 
 book_list = ["Bible", "The Headless Horseman", "The Gospel Driven Life", "On the Incarnation"]
 feast = ["Easter", "Christmas", "New Year", "Women's Day", "Men's Day"]
+feast_now: str
 now = datetime.datetime.now()
 
 def services(choice: str):
@@ -19,7 +20,7 @@ def services(choice: str):
             print("Sure, how can I assist you?")
 
         case "event":
-            print(f"It's ${feast} today! This is what we have: ")
+            print(f"It's ${feast_now} today! This is what we have: ")
             #more coming soon
 
         case _:
@@ -28,3 +29,5 @@ def services(choice: str):
 def bot():
     choice = input("How can I help you? ")
     services(choice)
+
+print(now)
