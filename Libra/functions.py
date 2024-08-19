8ba9bb2d5f6f1d9db5851e17837328c49bcc535e
jartsanull <langinenj1@gmail.com>
2024-08-19 16:55:48 +0300

    k

--- Libra/functions.py
+++ Libra/functions.py
@@ -28,9 +28,6 @@ def services(choice: str):
         case "assistance" | "assist" if "assistance" in choice or "assist" in choice:
             print("Sure, how can I assist you?")  #this is not the assistance you imagine
 
-        case "exit":
-            exit()
-
         case "event":
             print(f"It's ${feast_now} today! This is what we have: ")
             #more coming soon
