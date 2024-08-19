ff35d1650d18ca8bfd3e2128af2302286052b2c3
jartsanull <langinenj1@gmail.com>
2024-08-19 16:56:53 +0300

    Update functions.py

--- Libra/functions.py
+++ Libra/functions.py
@@ -21,7 +21,7 @@ def services(choice: str):
             print("\n".join(book_list))
 
         case "inquire" | "inquiry" if "inquire" in choice or "inquiry" in choice:
-            print("Hmmm... Let me see if we have it") #not done yet, need to add databases
+            print("Hmmm... Let me see if we have it") #not done yet, need to add database and functions
             time.sleep(5)
             print("Please wait...")
 
